5e592766624ff29dbb5bfd883b64d447d3a8b9c7
rahmanunabi <102538438+rahmanunabi@users.noreply.github.com>
2022-05-24 17:05:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+
+
+var a = prompt(" enter the number");
+if (a % 3 === 0) 
+{
+    console. log(alert("number is divisible by 3"));
+    } 
+    else 
+    {
+    console. log(alert("number is not divisible by 3"));
+    }
+
+    
+var b=prompt("enter any number");
+    if(b % 2 === 0){
+alert("the number is even");
+    }
+    else
+    {
+alert("the number is odd");
+    }
+
+
+var p = prompt(" enter the age");
+
+if(p > 18 ){
+alert("older enough");
+}
+else
+{
+alert("too younger");
+}
+
+
+
+var firstName = "abdur rahman";
+if (firstName === "abdur rahman") {
+ document.write("Hello abdur rahman!");
+}
+else
+{
+    document.write("you are not abdur rahman");
+}
+
+ var num1, num2;
+num1 = window.prompt("Input the First integer", );
+num2 = window.prompt("Input the second integer", );
+                                                 
+if(parseInt(num1, 10) > parseInt(num2, 10)) 
+  { 
+  console.log(alert( "The larger of "+ num1+ " and "+ num2+ " is "+ num1+ "." ));
+  }   
+else if(parseInt(num2, 10) > parseInt(num1, 10)) 
+  {
+  console.log( alert("The larger of "+ num1+" and "+ num2+ " is "+ num2+ "."));
+  }                  
+else
+  {
+   console.log(alert("The values "+ num1+ " and "+num2+ " are equal."));
+  }
+  
+  
+var num0 = parseInt(prompt("eneter any number"));
+if(num0 > 0)
+{
+alert("number is positive");
+}
+else if(num0 == 0)
+{
+alert("number is zero");
+}
+else 
+{
+alert("number is negative");
+}
+
+
+var n= parseInt(prompt("enter any Number, String & Boolean"));
+
+if(n > 0)
+{
+console.log(alert("that is number"));
+}
+else 
+{
+console.log(alert("that is string"));
+}
+
+
